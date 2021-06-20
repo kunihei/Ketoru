e521c242a204c8beffebd93c6b5a71f9aaba0451
kunihei <57933493+kunihei@users.noreply.github.com>
2021-06-21 08:37:09 +0900

    要件1~3を追加

--- Ketoru.playground/Contents.swift
+++ Ketoru.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class VirtualKetoru {
+    var boilingTimer:Timer = Timer()
+    var boilingCount = 0
+    var boilingSwich = 1
+    
+    func boilingFunction() {
+        if boilingSwich == 1 {
+            
+            boilingTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(boilingTime), userInfo: nil, repeats: true)
+        } else {
+            print("電源が入っていません")
+        }
+    }
+    
+    @objc func boilingTime() {
+        boilingCount += 1
+        if boilingCount == 5 {
+            print("お湯が沸きました！！")
+            boilingSwich = 0
+            boilingTimer.invalidate()
+            
+        }
+    }
+}
+
+let ketoru = VirtualKetoru()
+ketoru.boilingFunction()
